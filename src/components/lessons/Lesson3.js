a4c5a96d2beeb50d1f93ef9314e99ccd6e735713
elenalape <elenalape@github.com>
2021-04-01 12:04:07 +0100

    add lesson3 transcript

--- src/components/lessons/Lesson3.js
+++ src/components/lessons/Lesson3.js
@@ -3,18 +3,136 @@ import React from "react";
 const Lesson3 = () => {
 	return (
 		<div>
-			<h4>Summary</h4>
-			<p>
-				Coming soon...
-			</p>
-			<h4>
-				<a href="/">
-					Coming soon: Test your knowledge — complete the Phishing quiz!
-				</a>
-			</h4>
 			<h4>Transcript</h4>
+			<p>I have a confession to make. I committed a crime.</p>
+
 			<p>
-				Coming soon...
+				It was a really boring day at work and I wanted to have some
+				fun. I knew that my friend, Ben, was just about to graduate and
+				was waiting on some exam results.
+			</p>
+
+			<p>
+				So I crafted an email, pretending to be the university
+				department that is responsible for grades. I asked him to send
+				the university a photo of his passport, quite urgently, because
+				we’re having issues confirming his identity.
+			</p>
+
+			<p>
+				A few minutes later, Ben sends me his passport picture. So I
+				called him.
+			</p>
+
+			<p>
+				Of course, we’re friends, I’m not going to do anything terrible
+				with it besides maybe giggle at his hilarious passport photo,
+				but it could have totally been a criminal who wanted to take out
+				a loan on Ben’s behalf.
+			</p>
+
+			<p>
+				Due to security concerns, I’m not able to show you how exactly I
+				did this, but this technique of sending emails from a made up
+				email address is called <strong>spoofing</strong>.
+			</p>
+
+			<p>It’s a phishing technique. But what’s phishing?</p>
+
+			<p>
+				Phishing is the art of someone pretending to be someone else to
+				get things out of you. It’s illegal: it’s a crime. For example:
+			</p>
+
+			<ul>
+				<li>
+					someone pretending to be your bank to get to tell them your
+					card number
+				</li>
+				<li>
+					someone posing as an Amazon employee to get you give them
+					your account information
+				</li>
+				<li>
+					a user messaging you "on behalf of" Instagram support to get
+					you to give them your password, or your account will
+					disappear
+				</li>
+			</ul>
+			<p>
+				The idea of a phish is to create an email or a message that
+				looks like the real deal, so that you trust the sender, and
+				trust opening any links or attachments they send you, or answer
+				their questions.
+			</p>
+			<p>So how do we spot a phishing email?</p>
+			<p>Below are some tips to spot a potential phish.</p>
+			<ul>
+				<li>
+					Regognise an odd sender, where the email address doesn't
+					look quite right e.g. yourschool@gmail.com. However, email
+					addresses can be spoofed, so don't blindly trust the sender
+					if any of the following are also true.
+				</li>
+				<li>
+					If you didn’t expect to get that email or message, like a
+					bill for a service you never used, it could be a phishing
+					attempt.
+				</li>
+				<li>
+					Check if the organisation sent something similar in the
+					past. Formatting, links in the message. If it looks totally
+					different, be cautious.
+				</li>
+				<li>Phishes often have poor grammar and spelling.</li>
+				<li>
+					Mismatched URLs could be another giveaway. If the email
+					comes as on behalf of Facebook, but the link inside the
+					message asks you to visit some other site or send an email
+					that isn't facebook.com, it is likely to be a phish.
+				</li>
+			</ul>
+			<p>
+				Cool! But, what if you already clicked on a link in the message?
+				Is there anything you can do? (Yes, sometimes)
+			</p>
+			<ul>
+				<li>
+					Check the URL at the top of the page. Does it look like the
+					sender?
+				</li>
+				<li>
+					Be cautious of login forms that ask you to submit your data.
+					Banks, for example, will rarely link a login link directly
+					in the email. Instead, they ask you to go on their wesbite
+					yourself.
+				</li>
+			</ul>
+			<p>
+				As we saw at the beginning, successfully pretending to be
+				someone else is actually not that hard. Ben is a software
+				engineer with years of experience, yet still fell for the trap.
+			</p>
+
+			<p>
+				Usually, for high volumes of emails, our phishing detection
+				algorithms get better, attackers are also getting
+				increasingly... smart.
+			</p>
+
+			<p>
+				We forget what information we put out through social media. It
+				is totally possible that someone can call you, pretending to be
+				a friend you met at an event that you posted about, or reference
+				your birthday and home city.
+			</p>
+
+			<p>
+				Phishing is much more about people than it is about technology.
+			</p>
+			<p>
+				However, I hope that using the tips you learned throughout this
+				video, you will be able to protect yourself and those around you
 			</p>
 		</div>
 	);
